8ac5e55b518da3c9ca9bd77f56ed9f7df4c12aba
tonyrogers5 <125412893+tonyrogers5@users.noreply.github.com>
2023-03-02 19:32:14 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def send_message(message, user_message, is_private, username):
 
 
 def run_discord_bot():
-    TOKEN = "MTA3Njk0ODc1OTkzOTkyODA5NQ.GKMEb7.JYLtgdO2UFe4wbYYrSy1FnGdchFhXbf9m8bBpY"
+    TOKEN = "" #token deleted
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
